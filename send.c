6213c2803260720d2af84124a45734530fe9e225
Michael Hornung <mhornung@graznik.de>
2013-01-19 21:08:50 +0100

    Changed send.c to take command line arguments.

--- send.c
+++ send.c
@@ -2,8 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-	/* Group A, Switch 2, On */
-	socket_send(0, 0, 0, 1);
-
+	if (argc < 5) {
+		printf("Usage: %s DEV GROUP SOCKET DATA\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	/*FIXME: Check user input */
+	socket_send(argv[1], argv[2], argv[3], argv[4]);
 	return 0;
 }
